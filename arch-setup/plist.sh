995699f0e004e9dd322da645ff224b60afcb7ba4
Yiğit Çolakoğlu <yigitcolakoglu@hotmail.com>
2021-03-31 20:46:35 +0300

    Packages updated

--- arch-setup/plist.sh
+++ arch-setup/plist.sh
@@ -2,5 +2,5 @@
 
 #comm -23 <(pacman -Qqt | sort) <(pacman -Qqg base 2> /dev/null | sort) > ~/.dotfiles/arch-setup/packages.full
 pacman -Q | cut -f 1 -d " " | sort > ~/.dotfiles/arch-setup/packages.full
-comm -13 packages.blacklist packages.full > packages.minimal
+comm -13 ~/.dotfiles/arch-setup/packages.blacklist ~/.dotfiles/arch-setup/packages.full > ~/.dotfiles/arch-setup/packages.minimal
 
