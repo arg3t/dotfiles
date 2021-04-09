78800657a6bab267a25d8e3ebf0b1a442fda520a
Yigit Colakoglu <yigitcolakoglu@hotmail.com>
2021-04-09 10:10:15 +0300

    Added start page

---
+++ .local/share/startpage/src/index.js
@@ -0,0 +1,33 @@
+// //////// INDEX //////// //
+
+import truncateLinkName from './raisin/raisin.js'
+import displayClock from './clock/clock.js'
+import {buttonsContainer, searchContainer, toggleEngineIcons, sendSearch} from './search/search.js'
+import {generateButtons, cryptoButtonsContainer, getBinancedata, toggleButtons} from './binance/binance.js'
+import getOpenWeatherData from './openweather/openweather.js'
+import getUnsplashData from './unsplash/unsplash.js'
+import getStormglassData from './stormglass/stormglass.js'
+
+// Raisin
+truncateLinkName()
+
+// Clock
+displayClock()
+
+// Search
+buttonsContainer.addEventListener('click', toggleEngineIcons)
+searchContainer.addEventListener('keypress', sendSearch)
+
+// Binance
+generateButtons()
+getBinancedata()
+cryptoButtonsContainer.addEventListener('click', toggleButtons)
+
+// Openweather
+getOpenWeatherData()
+
+// Unsplash
+getUnsplashData()
+
+// Stormglass
+getStormglassData()
