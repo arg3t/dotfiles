ee8c71a9b4fa2fcd9a0ce9d5450e5e57099956bf
Yigit Colakoglu <yigit@yigitcolakoglu.com>
2024-01-21 11:49:24 +0100

    Removed doom config

--- .config/doom/cli.el
+++
@@ -1,7 +0,0 @@
-;;; cli.el -*- lexical-binding: t; -*-
-(setq org-confirm-babel-evaluate nil)
-
-(defun doom-shut-up-a (orig-fn &rest args)
-  (quiet! (apply orig-fn args)))
-
-(advice-add 'org-babel-execute-src-block :around #'doom-shut-up-a)
--- .config/doom/templates/template.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# `(buffer-name)`
-#
-# Created by `(user-full-name)` on `(format-time-string "%d/%m/%Y")`.
-# Copyright `user-mail-address`. `(format-time-string "%Y")`. All rights reserved.
-
